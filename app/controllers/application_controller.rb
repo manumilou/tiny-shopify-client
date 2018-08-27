879be72ed2e69a89482d3e11ef2ec119dea0f55e
Emmanuel Milou <emmanuel.milou@shopify.com>
2018-08-27 14:15:11 -0400

    Encrypt api credentials

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :setup_api_credentials
 
-  SHOPIFY_API_KEY = '03cf2a5a90fba6828a0b2129a6d0380b'
-  SHOPIFY_API_SECRET = 'f2ac1295544f1aa41b61fd5a9f94445f'
-  SHOPIFY_SHOP_NAME = 'shop1'
-
   attr_reader :shop_url
 
   private
 
   def setup_api_credentials
-    @shop_url = "https://#{SHOPIFY_API_KEY}:#{SHOPIFY_API_SECRET}@#{SHOPIFY_SHOP_NAME}.myshopify.io/admin"
+    api_key = Rails.application.secrets.shopify_api_key
+    api_secret = Rails.application.secrets.shopify_api_secret
+
+    @shop_url = "https://#{api_key}:#{api_secret}@shop1.myshopify.io/admin"
+
     ShopifyAPI::Base.site = @shop_url
   end
 end
